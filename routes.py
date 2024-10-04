34e67ef79280b89fe2be7961b5f41b04ea5fcd5b
Neigh1 <39903585+Neigh1@users.noreply.github.com>
2024-10-04 16:26:46 +0200

    Delete routes.py

--- routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-job_routes = Blueprint('job_routes', __name__)
-
-# Sample route
-@job_routes.route('/jobs', methods=['GET'])
-def get_jobs():
-    return jsonify({"jobs": "This is where jobs will be listed"})
